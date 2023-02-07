842b187777e29c6d50d7697fe3308a0fe11f5e8a
Muhd Nur Rejab <muhamadnurrejab@gmail.com>
2023-02-07 16:16:00 +0800

    first commit

---
+++ hello.py
@@ -0,0 +1,20 @@
+#Develop a software to calculate sales
+#1)Ask name of the user
+#2)Ask for how many percent is the sales tax
+#3)The output : "Hi, xxxx, the amount to pay is xxx, the sales is xxx "
+#sales amount = total sales + sales tax
+#Barang = Rm1,500 tax 6.25%, total amount = 1631.25
+
+#name = input("What is your name?\n")
+#goods = float(input ("How many is the amount to pay?\n"))
+#tax = float(input ("How many percent is the sales tax?\n"))
+#salesTax = tax/100 * goods
+#salesAmount = salesTax + goods
+#print("Hi " + name + ", the total amount to pay is RM" + str(salesAmount))
+
+#c-f
+
+name = input("What is your name?\n")
+celsius = float(input("What is your temperature in celsius?\n"))
+farenheit = (celsius * 1.8) + 32
+print("The temperature in farenheit is : " + str(farenheit))
