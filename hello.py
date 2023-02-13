c02ab0f98230679d28a1136d0e730e74c1f4b82e
Muhd Nur Rejab <muhamadnurrejab@gmail.com>
2023-02-13 14:22:09 +0800

    Perubahan Ketiga

--- hello.py
+++ hello.py
@@ -14,10 +14,26 @@
 
 #c-f
 
-name = input("What is your name?\n")
-celsius = float(input("What is your temperature in celsius?\n"))
-farenheit = (celsius * 1.8) + 32
-print("The temperature in farenheit is : " + str(farenheit))
+#name = input("What is your name?\n")
+#celsius = float(input("What is your temperature in celsius?\n"))
+#farenheit = (celsius * 1.8) + 32
+#print("The temperature in farenheit is : " + str(farenheit))
 
-name1 = input("What is your name?\n")
-name2 = input("What is your name?\n")
+#name1 = input("What is your name?\n")
+#name2 = input("What is your name?\n")
+
+#temperature = float(input("What is the temperature today?\n"))
+#if temperature > 80 :
+#    print("It's too hot!")
+#    print("Stay Inside")
+#elif temperature > 55 :
+#    print("Still quite hot!")  
+#print("Have a nice day")
+
+name = input("What is your name?\n")
+mark = float(input("Hi " + name + ", what is your mark?\n"))
+if mark >= 80 :
+    print("Hi " + name + ", your mark is " + str(mark) + ", you are brilliant!")
+elif mark <= 60 :
+    print("Hi " + name + ", your mark is " + str(mark) + ", you did okay")
+    print("Better luck next time :)")
